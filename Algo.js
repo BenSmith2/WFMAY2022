97bc6ac234a9836f45711afd99302a468c02e2d2
BenSmith2 <brsmith0224@gmail.com>
2022-05-18 11:50:28 -0400

    moved in directory

--- Algo.js
+++
@@ -1,8 +0,0 @@
-Var countPositives = 0
-Var numbers = [3,  4, -2, 7, 16, -8, 0];
-
-for (var i = 0; i < numbers.length; i++) {
-    
-}
-
-Console.log ("there are " + countPositives + ' psoitive values");
