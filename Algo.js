4564a4499bcdf5075ed7cba0af55cd4aa1b39884
BenSmith2 <brsmith0224@gmail.com>
2022-05-18 11:48:58 -0400

    workinjg on algo for the day

---
+++ Algo.js
@@ -0,0 +1,8 @@
+Var countPositives = 0
+Var numbers = [3,  4, -2, 7, 16, -8, 0];
+
+for (var i = 0; i < numbers.length; i++) {
+    
+}
+
+Console.log ("there are " + countPositives + ' psoitive values");
