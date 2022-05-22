e047b0cdeddd4d93ca9a50ba77c98dd78bab8a6a
BenSmith2 <brsmith0224@gmail.com>
2022-05-21 22:38:44 -0400

    Loop Challenges complete

---
+++ Day6/loopChallenges/loopChallenges.js
@@ -0,0 +1,65 @@
+// #1 print odds
+
+for (i = 0; i <= 20; i++){
+    if (i % 2 != 0) {
+        console.log(i)
+    }
+}
+
+i = 0
+
+while (i <= 20) {
+    if (i%2!=0) {
+        console.log(i)
+    }
+    i++;
+}
+// end 
+
+
+
+
+//#2 divisible by 3
+for (i = 100; i > 0; i--){
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+    }
+
+
+    i = 100
+
+    while (i > 0) {
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+        i--
+    }
+// end
+
+// #3 print the sequence
+
+for (i = 4; i > -5; i-=1.5){
+    console.log(i)
+}
+// end
+
+// #4 sigma
+
+var sum
+sum=0
+for (i = 0; i <= 100; i++){
+    sum=sum+i
+} console.log(sum)
+// end
+
+// #5 product 
+var product
+product=1
+for (i = 1; i <= 12; i++){
+    product = i * product;
+}
+console.log(product)
+// end 
+
+
