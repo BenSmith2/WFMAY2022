13bbd829daae88940c938a21c5f15f5815b0fff7
BenSmith2 <brsmith0224@gmail.com>
2022-05-24 21:06:48 -0400

    init commit likes part ii

--- likingLikes/likingLikesParti/script.js
+++ likingLikes/likingLikesParti/script.js
@@ -4,6 +4,8 @@ console.log(num1)
 var likes1 = document.querySelector("#likes")
 
 function btn1() {
-    num1++;
+    while (num1 < 4){
+        num1++;
+    }
     likes1.innerText = num1 + ' Like(s)';
 }
