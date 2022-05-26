f58cf4f8d850520f12f559e3492ed1a347976eeb
BenSmith2 <brsmith0224@gmail.com>
2022-05-26 11:39:39 -0400

    Algo and day 8 folder commit

---
+++ Day8/Day8Algo.js
@@ -0,0 +1,8 @@
+function flatten(arr2d) {
+    var flat = [];
+    // your code here
+    return flat;
+}
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
